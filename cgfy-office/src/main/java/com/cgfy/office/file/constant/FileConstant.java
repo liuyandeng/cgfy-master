5e66371ef9c076a1a79f466f9593dbc04aa20a18
liuyandeng <877507054@qq.com>
2020-11-23 15:29:56 +0800

    bug修复

--- cgfy-office/src/main/java/com/cgfy/office/file/constant/FileConstant.java
+++ cgfy-office/src/main/java/com/cgfy/office/file/constant/FileConstant.java
@@ -2,7 +2,7 @@ package com.cgfy.office.file.constant;
 
 public class FileConstant {
 
-  public static final String PREVIEW_REMOTE_URL = "http://localhost:8080/preview/convertFile";
+  public static final String PREVIEW_REMOTE_URL = "http://localhost:8089/preview/convertFile";
 
   public static final String FILE_PARAM_KEY = "file";
 

3e0f71d862941642adb79119413427e43fdde94a
liuyandeng <877507054@qq.com>
2020-12-02 21:28:47 +0800

    表单提交案例

---
+++ cgfy-socket/src/main/java/com/cgfy/socket/api/bean/UserInfo.java
@@ -0,0 +1,13 @@
+package com.cgfy.socket.api.bean;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+    private  String id;
+    private  String name;
+    private  String age;
+    private  String sex;
+    private  String address;
+
+}
--- cgfy-socket/src/main/java/com/cgfy/socket/api/controller/TestController.java
+++ cgfy-socket/src/main/java/com/cgfy/socket/api/controller/TestController.java
@@ -1,17 +1,61 @@
 package com.cgfy.socket.api.controller;
+import com.alibaba.fastjson.JSON;
+import com.cgfy.socket.api.bean.UserInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
 
 @Controller
 public class TestController {
-
     @GetMapping(value = "/")
     public String toIndex(Model model) {
-        model.addAttribute("msg","欢迎来到我的世界");
+        List<UserInfo> userList=new ArrayList();
+        UserInfo user1 =new UserInfo();
+        user1.setId("001");
+        user1.setName("张三");
+        user1.setAge("20");
+        user1.setSex("男");
+        user1.setAddress("河南");
+        UserInfo user2 =new UserInfo();
+        user2.setId("002");
+        user2.setName("李四");
+        user2.setAge("25");
+        user2.setSex("女");
+        user2.setAddress("山西");
+        UserInfo user3 =new UserInfo();
+        user3.setId("003");
+        user3.setName("王五");
+        user3.setAge("30");
+        user3.setSex("未知");
+        user3.setAddress("河北");
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        model.addAttribute("msg","欢迎来到我的世界!!!");
+        model.addAttribute("flag",true);
+        model.addAttribute("date",new Date());
+        model.addAttribute("userList",userList);
         return "index";
     }
 
+    /**
+     * 测试提交
+     * @param user
+     * @return
+     */
+    @PostMapping(value = "/testSubmit")
+    @ResponseBody
+    public Map testSubmit(UserInfo user) {
+        Map map=new HashMap();
+        System.out.println(user.getName());
+        map.put("msg",user.getName());
+        return map;
+    }
 
 }
 

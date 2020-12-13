22474f261480f44fbba819900bc63bce1f3bb417
liuyandeng <877507054@qq.com>
2020-12-13 21:06:17 +0800

    for update

--- cgfy-lock/src/main/java/com/cgfy/lock/java/SyncThread.java
+++ cgfy-lock/src/main/java/com/cgfy/lock/java/SyncThread.java
@@ -34,6 +34,7 @@ public class SyncThread implements Runnable{
      * 创建了两个SyncThread的对象syncThread1和syncThread2，线程thread1执行的是syncThread1对象中的synchronized代码(run)，
      * 而线程thread2执行的是syncThread2对象中的synchronized代码(run)；我们知道synchronized锁定的是对象，
      * 这时会有两把锁分别锁定syncThread1对象和syncThread2对象，而这两把锁是互不干扰的，不形成互斥，所以两个线程可以同时执行。
+     * 此处思考一个问题：为什么分布式环境下synchronized失效？如何解决这种情况？
      * @param args
      */
     public static void main(String[] args) {
@@ -43,4 +44,14 @@ public class SyncThread implements Runnable{
         thread1.start();
         thread2.start();
     }
+
+//         new Thread(test::method2).start();的简洁写法
+//        //替换下面片段
+//        new Thread(new Runnable() {
+//        @Override
+//        public void run() {
+//            test.method2();
+//        }
+//    }).start();
+
 }
